8a7324a15d173a9015df42de082aa1de9243c8ff
Nozomu MIURA <miura@digitalstage.jp>
2015-12-06 15:46:34 +0800

    Initial Commit

---
+++ INetSyphon_OSX_SDK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  INetSyphon_OSX_SDK
+//
+//  Created by Nozomu MIURA on 2015/12/06.
+//  Copyright © 2015年 TECHLIFE SG Pte.Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ INetSyphon_OSX_SDK/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  INetSyphon_OSX_SDK
+//
+//  Created by Nozomu MIURA on 2015/12/06.
+//  Copyright © 2015年 TECHLIFE SG Pte.Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
