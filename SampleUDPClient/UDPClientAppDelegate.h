f07d01cbad2bf02f30b66da43a1f9ab62cd308f7
Nozomu MIURA <miura@digitalstage.jp>
2015-12-12 16:31:35 +0800

    Fixed client connection problem

---
+++ SampleUDPClient/UDPClientAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleUDPClient
+//
+//  Created by Nozomu MIURA on 2015/12/12.
+//  Copyright © 2015年 TECHLIFE SG Pte.Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
